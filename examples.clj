d91d51d41c9a51a96932e3e25f69acb6c7fb932f
yilmazedis <ylmazedis@gmail.com>
2017-10-06 14:25:58 +0300

    Update examples.clj

--- examples.clj
+++ examples.clj
@@ -1,39 +1,53 @@
-(ns clojure.examples.hello
+(ns clojure.examples.example
+   (:require [clojure.set :as set])
+   (require [clojure.string :as str])
+   
    (:gen-class))
- 
- (import java.util.ArrayList)
 
-(defn lines []
-   (with-open [rdr (clojure.java.io/reader "dictionary1.txt")]
-   (reduce conj [] (line-seq rdr))))
+(defn readFile [name]
+  (clojure.string/split (slurp name) #"\s+"))
  
- ;(println (nth (clojure.string/split (nth (lines) 0) #" ") 2))
- ;(def x (clojure.string/split (nth (lines) 0) #" "))
- ;(println (count x))
- 
- (defn TraceWords []
-   
-   (println (.charAt "yilmaz" 3))
-   (println (.toString 23234))
-   (println (java.util.ArrayList. (lines
-                                    )))
-   
-   (def words (lines))
-   (def wordNum (count words))
-   ;(println wordNum)
-   (dotimes [n wordNum]
-   (def singleLine (clojure.string/split (nth words n) #" "))
-   
-   (def totalWord (count singleLine))
-   (println "\nline size --->>>> " totalWord)
-   (dotimes [nm totalWord]
-   (def newWord (clojure.string/split (nth singleLine nm) #" "))
-   
-   (println newWord))))
- 
- (TraceWords)
- 
- ;(defn examp []
-  ;(clojure.string/split "hello\ndfsdf"  #"\n"))
+ (defn mkList [vect]
+  ;(println vect)
+	(def reverseVect (reverse vect)) ; reverse vector
+	(def newList (list)) ; Decleration empty list
+	(def sizeVect (count vect)) ; vector size
+	(dotimes [n sizeVect] ; Loop statement
+		(def elemVect (nth reverseVect n)) ; get element of vector
+		(def letters (apply list elemVect)) ; convert vector to list
+		(def newList (cons letters newList))) ; add list to begining of list
 
-;(println (examp))
+	(let [] newList)
+)
+
+;(println (mkList (readFile "document1.txt")))
+;(println (mkList (readFile "dictionary1.txt")))
+
+(defn matchWords [w1 w2]
+  (def matched 0)
+  (def len_w1 (count w1))
+  (def len_w2 (count w2))
+  (dotimes [n1 len_w1]
+      (dotimes [n2 len_w2]
+          (if (= (nth w1 n1) (nth w2 n2)) (def matched (inc matched)))))
+  (let [] matched))
+
+
+(println (matchWords (mkList (readFile "document2.txt")) (mkList (readFile "dictionary1.txt"))))
+
+(defn ints-from [n]
+  (cons n (lazy-seq (ints-from (dec n)))))
+
+(println (take 10 (ints-from 7)))
+
+
+(defn rotations [a-seq]
+  (map concat (tails a-seq) (inits a-seq)))
+
+
+(rotations (range 5))
+
+
+
+
+
